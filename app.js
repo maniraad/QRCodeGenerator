6f0940888c418a9ce0468e8ed95bb066ccfdbbb2
maniraad <maniraad7@gmail.com>
2023-11-13 22:00:59 +0330

    create animation active btn

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ const img = $.querySelector('img');
 
 
 const showQRCode = () => {
-    let inputValue = input.value.trim()
+    let inputValue = input.value
+    if (inputValue) {
+        generatorBtn.innerHTML = "Generating QR Coding"
 
-    img.setAttribute('src', `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${inputValue}`)
-    img.addEventListener('load', () => {
-        card.classList.add('active')
-    })
+        img.setAttribute('src', `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${inputValue}`)
+        img.addEventListener('load', () => {
+            card.classList.add('active')
+            generatorBtn.innerHTML = "Generated"
+        })
+    }
 }
 
 generatorBtn.addEventListener('click', showQRCode);
